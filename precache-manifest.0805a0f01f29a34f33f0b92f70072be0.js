d9cbd97658e29be10cb16fe263e92475ab3bae35
Tony Fu <ohmygod525@gmail.com>
2020-09-28 20:53:16 +0800

    Updates

---
+++ precache-manifest.0805a0f01f29a34f33f0b92f70072be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5630f5f69b327fe3e64e4d4ad9f1a73",
+    "url": "/nextTRA/index.html"
+  },
+  {
+    "revision": "d2bead78b13d44f82ec7",
+    "url": "/nextTRA/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5cc36c0d0ce4008aacd9",
+    "url": "/nextTRA/static/js/2.219c9d2e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/nextTRA/static/js/2.219c9d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bead78b13d44f82ec7",
+    "url": "/nextTRA/static/js/main.c0b3a08d.chunk.js"
+  },
+  {
+    "revision": "5453cf9c4dafa80f5582",
+    "url": "/nextTRA/static/js/runtime-main.672f0d78.js"
+  }
+]);
